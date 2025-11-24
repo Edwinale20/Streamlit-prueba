ce059a061e8f749d8ddf28bd00274b41c28a75a6
Dmels <136374991+Edwinale20@users.noreply.github.com>
2025-11-23 20:35:33 -0600

    Update Prueba.py

--- Prueba.py
+++ Prueba.py
@@ -3,8 +3,6 @@ import pandas as pd
 
 st.title("Dashboard conectado a OneDrive 🔥")
 
-url = "https://7eleven-my.sharepoint.com/:f:/g/personal/edwing_cardenas_7-eleven_com_mx/ElsK3j-g6u5Gly9lUEeKMEMBELmofLCv22h-GNeKCaq0LA?e=kPoOQ7"
-
 st.write("Leyendo archivo desde OneDrive empresarial...")
 
 df = pd.read_excel(url)
